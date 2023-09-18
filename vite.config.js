a0f3aabc7f9d23cceb47491c10ef25980d35a4b0
PhamTinh1904 <phamtinhpy2017@gmail.com>
2023-09-18 21:58:02 +0700

    Build File Img Tours

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    
-  },'.js': 'jsx',
-  base: "/"
-
-})
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name].[ext]",
+      },
+    },
+  },
+  esbuild: {},
+  ".js": "jsx",
+  base: "/",
+});
